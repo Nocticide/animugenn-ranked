8ba2a72409a964ad6b1220a537991c9960b0aa7d
/nocticide <72424187+Nocticide@users.noreply.github.com>
2022-09-19 01:13:49 -0400

    test frame

--- Client.lua
+++ Client.lua
@@ -1,1 +1,11 @@
-print("Handler")
+local Player = game.Players.LocalPlayer
+PlayerGui = Player.PlayerGui
+
+local RankedScreen = Instance.new("ScreenGui", PlayerGui)
+RankedScreen.Name = "RankedScreen"
+RankedScreen.IgnoreGuiInset = true
+RankedScreen.ResetOnSpawn = false
+RankedScreen.DisplayOrder = 50
+local MasterGroup = Instance.new("CanvasGroup", RankedScreen)
+MasterGroup.Name = "MasterFrame"
+MasterGroup.Size = UDim.new(1,0,1,0)
