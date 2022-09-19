573cbc6002f81e59279103f8620a7363cf0e8361
/nocticide <72424187+Nocticide@users.noreply.github.com>
2022-09-19 01:52:05 -0400

    bg fade

--- Client.lua
+++ Client.lua
@@ -1,13 +1,21 @@
-local Player = game.Players.LocalPlayer
+local TweenService = game:GetService("TweenService")
+TweenService = game:GetService("TweenService")
+
+Player = game.Players.LocalPlayer
 PlayerGui = Player.PlayerGui
 
 warn(Player.Name.." ranked GUI instantiated.")
 
-local RankedScreen = Instance.new("ScreenGui", PlayerGui)
+RankedScreen = Instance.new("ScreenGui", PlayerGui)
 RankedScreen.Name = "RankedScreen"
 RankedScreen.IgnoreGuiInset = true
 RankedScreen.ResetOnSpawn = false
 RankedScreen.DisplayOrder = 50
-local MasterGroup = Instance.new("CanvasGroup", RankedScreen)
+RankedScreen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+MasterGroup = Instance.new("CanvasGroup", RankedScreen)
 MasterGroup.Name = "MasterFrame"
 MasterGroup.Size = UDim2.new(1,0,1,0)
+MasterGroup.BackgroundColor3 = Color3.fromRGB(148, 3, 3)
+
+TweenService:Create(MasterGroup, (TweenInfo.new(30, Enum.EasingStyle.Quint, Enum.EasingStyle.Out)), {BackgroundColor3 = Color3.fromRGB(0,0)}):Play() -- BG Fade
