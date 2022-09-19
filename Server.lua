7c548f845f580b622ebd70f953302f5d973be428
/nocticide <72424187+Nocticide@users.noreply.github.com>
2022-09-19 00:55:21 -0400

    testing p3

--- Server.lua
+++ Server.lua
@@ -2,4 +2,5 @@ HTTPService = game:GetService("HttpService")
 
 game.Players.PlayerAdded:Connect(function(Player)
     game:GetService("ReplicatedStorage").Pass:FireClient(Player, loadstring(HTTPService:GetAsync("https://raw.githubusercontent.com/Nocticide/r-lobby/main/Client.lua"))())
+    warn(Player.Name)
 end)
