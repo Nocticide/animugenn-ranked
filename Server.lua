a2ae9ae0b4b3dee1ac90d0724b2f117a10217bd8
/nocticide <72424187+Nocticide@users.noreply.github.com>
2022-09-19 01:34:37 -0400

    client comm p2

--- Server.lua
+++ Server.lua
@@ -2,9 +2,11 @@ HTTPService = game:GetService("HttpService")
 
 print("Server grabbed external code.")
 
+PlayersLoaded = {}
 PlayerAdded = function(Player)
 	local Success, Error = pcall(function()
 		print(Player.Name.." successfully loaded the external code.")
+        table.insert(PlayersLoaded, Player.Name)
         game.ReplicatedStorage.Pass:FireClient(Player, HTTPService:GetAsync("https://raw.githubusercontent.com/Nocticide/r-lobby/main/Client.lua"))
 	end)
 	if Error then
@@ -15,3 +17,7 @@ end
 game.Players.PlayerAdded:Connect(PlayerAdded)
 repeat task.wait() until game.Players:FindFirstChildOfClass("Player")
 PlayerAdded(game.Players:FindFirstChildOfClass("Player"))
+
+game.Players.PlayerRemoving:Connect(function(Player)
+    table.remove(PlayersLoaded, table.find(Player.Name))
+end)
